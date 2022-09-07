98bfe8cba5b1905addea1deb828de3fa3adfa875
gBeckerBitpioneers <113032687+gBeckerBitpioneers@users.noreply.github.com>
2022-09-07 15:26:55 +0200

    Update plausible_analytics.dart
    Added tracking of os and os version

--- lib/plausible_analytics.dart
+++ lib/plausible_analytics.dart
@@ -2,6 +2,7 @@ library plausible_analytics;
 
 import 'package:universal_io/io.dart'; // instead of 'dart:io';
 import 'dart:convert';
+import 'package:device_info_plus/device_info_plus.dart';
 
 /// Plausible class. Use the constructor to set the parameters.
 class Plausible {
@@ -36,7 +37,20 @@ class Plausible {
     referrer = "app://localhost/" + referrer;
 
     // Get and set device infos
-    String version = Platform.operatingSystemVersion.replaceAll('"', '');
+    String version = "";
+
+    if (Platform.isAndroid) {
+      var androidInfo = await DeviceInfoPlugin().androidInfo;
+      var release = androidInfo.version.release;
+      version = "Android $release";
+    } else if (Platform.isIOS) {
+      var iosInfo = await DeviceInfoPlugin().iosInfo;
+      var systemName = iosInfo.systemName;
+      var version = iosInfo.systemVersion;
+      version = "$systemName $version";
+    } else {
+      version = Platform.operatingSystemVersion.replaceAll('"', '');
+    }
 
     if (userAgent == "") {
       userAgent = "Mozilla/5.0 ($version; rv:53.0) Gecko/20100101 Chrome/53.0";
@@ -70,3 +84,4 @@ class Plausible {
     return 1;
   }
 }
+
